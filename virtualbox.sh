f219dc4be28d5d972a750034d7acdb1beb51464d
Michael Feher <PersonalPC@live.com>
2014-08-16 22:30:03 -0600

    no push to dev null

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 wget http://download.virtualbox.org/virtualbox/4.1.34/virtualbox-4.1_4.1.34-95024~Ubuntu~precise_i386.deb
-sudo dpkg -i virtualbox-4.1_4.1.34-95024~Ubuntu~precise_i386.deb > /dev/null 2>&1
-
-if [ $? -gt 0 ]; then
-   sudo apt-get -f --force-yes --yes install>/dev/null 2>&1
-fi
+sudo dpkg -i virtualbox-4.1_4.1.34-95024~Ubuntu~precise_i386.deb
+sudo apt-get -f --force-yes --yes install
 # This is needed to complet vb installation
-dpkg -i virtualbox-4.1_4.1.34-95024~Ubuntu~precise_i386.deb > /dev/null 2 > &1
+dpkg -i virtualbox-4.1_4.1.34-95024~Ubuntu~precise_i386.deb
