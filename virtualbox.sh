5873e6fe46b3b8fe3f1ced43a094f7dd0683e815
Michael Feher <PersonalPC@live.com>
2014-08-16 22:39:23 -0600

    no message

--- virtualbox.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-wget http://download.virtualbox.org/virtualbox/4.1.34/virtualbox-4.1_4.1.34-95024~Ubuntu~precise_i386.deb
-sudo dpkg -i virtualbox-4.1_4.1.34-95024~Ubuntu~precise_i386.deb
-sudo apt-get -f --force-yes --yes install
-# This is needed to complet vb installation
-dpkg -i virtualbox-4.1_4.1.34-95024~Ubuntu~precise_i386.deb
