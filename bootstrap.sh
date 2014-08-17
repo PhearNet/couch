d4d3389c8a3460e58ac2b625e61748444385fbfe
Michael Feher <PersonalPC@live.com>
2014-08-16 22:17:28 -0600

    debug

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y couchdb nginx nodejs node
-cp /vagrant/system/default.ini /etc/couchdb/
-service couchdb restart
+#apt-get update
+#apt-get install -y couchdb nginx nodejs node
+#cp /vagrant/system/default.ini /etc/couchdb/
+#service couchdb restart
